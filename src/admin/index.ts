58c3d743568c57ac11c41644a9c4af4cdf419788
NetSpud <edward@themorrisagency.co.uk>
2021-06-08 18:49:56 +0100

    Added authenticated routes

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,8 +1,23 @@
 import express from "express";
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send(`admin!`);
+import { loggedIn } from "../utils/middlewares";
+import csurf from "csurf";
+router.get("/", loggedIn, csurf(), (req, res) => {
+  res.render("admin", {
+    csrf: req.csrfToken(),
+  });
 });
+router.get("/logout", loggedIn, (req, res) => {
+  req.session.destroy((err) => {
+    res.send(err);
+  });
+});
+
+router.get("/item", (req, res) => {
+  res.render("item");
+});
+
+import apiRoute from "../api";
+router.use("/api", apiRoute);
 
 export default router;
