e965205ec5411506bc7d0c17ded8f36f648c3b0d
NetSpud <edward@themorrisagency.co.uk>
2021-06-08 18:44:40 +0100

    Added MySQL Store for sessions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import session from "express-session";
+const MySQLStore = require("express-mysql-session")(session);
 dotenv.config();
+
+var options = {
+  port: 3306,
+  host: process.env.HOST || "localhost",
+  user: process.env.USER || "root",
+  password: process.env.PASS || "123",
+  database: process.env.DB || "storage",
+  checkExpirationInterval: 900000,
+};
+
+const store = new MySQLStore(options);
+
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
@@ -13,17 +26,22 @@ app.use(
     saveUninitialized: true,
     cookie: { secure: false },
     name: `_session`,
+    store: store,
   })
 );
 
 declare module "express-session" {
   export interface Session {
     setup: boolean;
+    userID: string;
+    email: string;
+    accountLevel: string;
   }
 }
 
 import adminRoute from "./admin";
 import setupRoute from "./setup";
+import loginRoute from "./login";
 app.use("/admin", adminRoute);
 
 app.get("/", (req, res) => {
@@ -32,6 +50,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/setup", setupRoute);
+app.use("/login", loginRoute);
 
 const port = process.env.PORT || 3000;
 
