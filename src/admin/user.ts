3c6e29971339a881669cb94f7a8d90f675530687
NetSpud <edward@themorrisagency.co.uk>
2021-06-09 16:02:12 +0100

    Added user inviting and checking

---
+++ src/admin/user.ts
@@ -0,0 +1,25 @@
+import express from "express";
+const router = express.Router();
+import csurf from "csurf";
+import User from "../api/v1/classes/user";
+router.get("/", csurf(), (req, res) => {
+  res.render("user", {
+    csrf: req.csrfToken(),
+  });
+});
+
+router.post("/invite", (req, res) => {
+  const user = new User();
+
+  user
+    .exists(req.body.email)
+    .then((d) => {
+      return user.invite(req.body.email);
+    })
+    .then(() => {
+      res.json({ success: true });
+    })
+    .catch((err) => res.json({ error: String(err) }));
+});
+
+export default router;
