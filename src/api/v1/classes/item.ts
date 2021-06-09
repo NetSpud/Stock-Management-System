a7651e6a01dc99f930cf4d504ed9a223027b060e
NetSpud <edward@themorrisagency.co.uk>
2021-06-09 14:14:20 +0100

    Moved Item class to be in a separate file

---
+++ src/api/v1/classes/item.ts
@@ -0,0 +1,75 @@
+import con from "../../../utils/SQL";
+import { v4 as uuidv4 } from "uuid";
+
+export default class Item {
+  _userID: string;
+
+  constructor(userID: string) {
+    this._userID = userID;
+  }
+  create(name: string, quantity: string) {
+    return new Promise((resolve, reject) => {
+      var sql =
+        "INSERT INTO item (id, name, quantity, userID) VALUES (?,?,?,?)";
+      con.query(
+        sql,
+        [uuidv4(), name, quantity, this._userID],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(true);
+        }
+      );
+    });
+  }
+  delete(itemID: string) {
+    return new Promise((resolve, reject) => {
+      var sql = "DELETE FROM item where id = ? and userID = ?";
+      con.query(sql, [itemID, this._userID], (err, result) => {
+        if (err) reject(err);
+        if (result.affectedRows > 0) {
+          resolve(true);
+        } else {
+          reject(`Something went wrong while trying to delete the item. `);
+        }
+      });
+    });
+  }
+  get all() {
+    return new Promise<{ id: string; name: string; quantity: string }>(
+      (resolve, reject) => {
+        con.query(
+          "SELECT * FROM item where userID = ?",
+          [this._userID],
+          (err, result, fields) => {
+            if (err) reject(err);
+            resolve(result);
+          }
+        );
+      }
+    );
+  }
+  single(id: string) {
+    return new Promise<{ id: string; name: string; quantity: string }>(
+      (resolve, reject) => {
+        con.query(
+          "SELECT * FROM item where id = ? and userID = ?",
+          [id, this._userID],
+          (err, result, fields) => {
+            if (err) reject(err);
+            resolve(result[0]);
+          }
+        );
+      }
+    );
+  }
+  update(id: string, name: string, quantity: string) {
+    return new Promise((resolve, reject) => {
+      var sql =
+        "UPDATE item SET name = ?, quantity = ? WHERE id = ? and userID = ?";
+      con.query(sql, [name, quantity, id, this._userID], (err, result) => {
+        if (err) reject(err);
+        resolve(true);
+      });
+    });
+  }
+}
