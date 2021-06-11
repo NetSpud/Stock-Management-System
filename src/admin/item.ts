cbd15458dd37e7c8de9b4ac76611283b39508edf
NetSpud <edward@themorrisagency.co.uk>
2021-06-11 13:11:12 +0100

    Add email to rendering on topbar

--- src/admin/item.ts
+++ src/admin/item.ts
@@ -5,12 +5,14 @@ import csurf from "csurf";
 router.get("/", csurf(), (req, res) => {
   res.render("item", {
     csrf: req.csrfToken(),
+    email: req.session.email,
   });
 });
 
 router.get("/edit/:id", csurf(), (req, res) => {
   res.render("itemEdit", {
     csrf: req.csrfToken(),
+    email: req.session.email,
   });
 });
 
